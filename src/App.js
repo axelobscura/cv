896629f73be6c71a4da7d1fe7f37f3d28ca2c3e7
Axel <axosar@gmail.com>
2019-10-25 13:53:14 -0500

    Agregar bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container>
+          <Row>
+            <Col>1 of 2</Col>
+            <Col>2 of 2</Col>
+          </Row>
+          <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
