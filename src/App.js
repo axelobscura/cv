a67ad55d0aa3db7d14147f99ea3f979a245cb1c1
Axel <axosar@gmail.com>
2019-10-25 19:11:48 -0500

    El hover en accion

--- src/App.js
+++ src/App.js
@@ -14,29 +14,24 @@ function App() {
       transform: `scale(${styles.scale})`,
     };
   }
-  // wrap the `spring` helper to use a bouncy config
   function bounce(val) {
     return spring(val, {
       stiffness: 330,
-      damping: 22,
+      damping: 100,
     });
   }
-  // child matches will...
   const bounceTransition = {
-    // start in a transparent, upscaled state
     atEnter: {
       opacity: 0,
-      scale: 1.2,
+      scale: 0.1,
     },
-    // leave in a transparent, downscaled state
     atLeave: {
-      opacity: bounce(0),
-      scale: bounce(0.8),
+      opacity: 0,
+      scale: 0.1,
     },
-    // and rest at an opaque, normally-scaled state
     atActive: {
-      opacity: bounce(1),
-      scale: bounce(1),
+      opacity: 1,
+      scale: 1,
     },
   };
   return (
@@ -53,10 +48,12 @@ function App() {
                   <div className='green-column'>
                     <Switch>
                       <AnimatedSwitch
-                        atEnter={bounceTransition.atEnter}
-                        atLeave={bounceTransition.atLeave}
-                        atActive={bounceTransition.atActive}
-                        mapStyles={mapStyles}
+                        atEnter={{ offset: +100 }}
+                        atLeave={{ offset: +100 }}
+                        atActive={{ offset: 0 }}
+                        mapStyles={(styles) => ({
+                          transform: `translateY(${styles.offset}%)`,
+                        })}
                         className="switch-wrapper"
                       >
                         <Route 
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -6,11 +6,11 @@ const Menu = (props) => {
     return(
         <div className="menu">
             <ul>
-                <li><Link to="/"><i class="material-icons">face</i></Link></li>
-                <li><Link to="/datos"><i class="material-icons">face</i></Link></li>
-                <li><i class="material-icons">face</i></li>
-                <li><i class="material-icons">face</i></li>
-                <li><i class="material-icons">face</i></li>
+                <li><Link to="/"><i class="material-icons">change_history</i></Link></li>
+                <li><Link to="/datos"><i class="material-icons">eco</i></Link></li>
+                <li><Link to="/datos"><i class="material-icons">donut_large</i></Link></li>
+                <li><Link to="/datos"><i class="material-icons">explore</i></Link></li>
+                <li><Link to="/datos"><i class="material-icons">fingerprint</i></Link></li>
             </ul>
             
         </div>
