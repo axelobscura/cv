58cfcd74cc5a988dcdb7903e2324c00d19859170
Axel <axosar@gmail.com>
2019-12-26 17:26:37 -0600

    Primera implementacion de api

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import './Home.css';
 
 const Home = () => {
+    //https://i.picsum.photos/id/327/400/400.jpg
+    const [count, setCount] = useState(1);
+    const [data, setData] = useState({ hits: [] });
+    useEffect(async () => {
+        const result = await axios(
+          'https://i.picsum.photos/id/327/400/400.jpg',
+        );
+        setData(result.data);
+      }, []);
+    
     return (
         <div className="home">
             <Link to="/datos"><h1>Full Stack Developer</h1></Link>
             <h2>Axel Laurent Obscura Sarzotti</h2>
             <p>Coyoacán, México</p>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount((Math.floor(Math.random() * 1000)))}>
+                Click me
+            </button>
+            <ul>
+            {data.hits.map(item => (
+                <li key={item.objectID}>
+                    <a href={item.url}>{item.title}</a>
+                </li>
+            ))}
+            </ul>
         </div>
     )
 }
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -6,11 +6,11 @@ const Menu = (props) => {
     return(
         <div className="menu">
             <ul>
-                <li><Link to="/"><i class="material-icons">change_history</i></Link></li>
-                <li><Link to="/datos"><i class="material-icons">eco</i></Link></li>
-                <li><Link to="/datos"><i class="material-icons">donut_large</i></Link></li>
-                <li><Link to="/datos"><i class="material-icons">explore</i></Link></li>
-                <li><Link to="/datos"><i class="material-icons">fingerprint</i></Link></li>
+                <li><Link to="/"><i className="material-icons">change_history</i></Link></li>
+                <li><Link to="/datos"><i className="material-icons">eco</i></Link></li>
+                <li><Link to="/datos"><i className="material-icons">donut_large</i></Link></li>
+                <li><Link to="/datos"><i className="material-icons">explore</i></Link></li>
+                <li><Link to="/datos"><i className="material-icons">fingerprint</i></Link></li>
             </ul>
             
         </div>
