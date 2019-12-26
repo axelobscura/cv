d57cdd1881f7920df98c14487d43f848d487666b
Axel <axosar@gmail.com>
2019-12-26 16:14:11 -0600

    Arreglar menu

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <Link to="/datos"><h1>Full Stack Developer</h1></Link>
             <h2>Axel Laurent Obscura Sarzotti</h2>
             <p>Coyoacán, México</p>
