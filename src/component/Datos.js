54bd24bea7dba53c86afb0b22707cd1b9a64cb05
Axel <axosar@gmail.com>
2019-12-27 16:05:11 -0600

    Mensajes

--- src/component/Datos.js
+++ src/component/Datos.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import data from "../data/data"; 
+import data from "../data/data";
+import './Datos.css';
 
+const profile = data.profile;
 const socialMediaList = data.SocialMedias;
 
 const Datos = () => {
     return(
-        <div>
-            <h2>datos</h2>
-            <p>si claro</p>
+        <div className="datos">
+            {profile.map(p => (
+                <div>
+                    <h2>{p.name}</h2>
+                    <p className="p-0 m-0">{p.role}</p>
+                    <h3>{p.email}</h3>
+                    <p>{p.phone}</p>
+                    <hr/>
+                    <h1>PROFESSIONAL SUMMARY</h1>
+                    <p>{p.summary}</p>
+                </div>
+            ))}
+            {/* 
             <ul>
                 {socialMediaList.map(s => (<li>{s}</li>))}
             </ul>
@@ -61,6 +73,7 @@ const Datos = () => {
                   })
                 } 
             </div>
+            */}
         </div>
         
     )
