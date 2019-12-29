6bc2c60e08d6e161ae83f974345a110629f89ec4
Axel <axosar@gmail.com>
2019-12-29 17:20:41 -0600

    Estilos a flechas

--- src/component/Noticias.js
+++ src/component/Noticias.js
@@ -25,7 +25,7 @@ const Noticias = (props) => {
                 speed={props.speed}
                 >
                 {(index) => (
-                    <props.tag> -{quotes[rand(0, quotes.length - 1)]}- </props.tag>
+                    <props.tag style={{ whiteSpace: "nowrap" }}> <i class="lni-chevron-right" style={{marginLeft: '10px', color: 'red'}}></i> {quotes[rand(0, quotes.length - 1)]} </props.tag>
                 )}
             </Ticker>
         </div>
