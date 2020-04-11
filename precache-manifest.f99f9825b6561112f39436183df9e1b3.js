b0935e6092bac2ec0448192da56ea5b82f0429c0
Axel Obscura <axosar@gmail.com>
2020-04-10 20:04:07 -0500

    Updates

--- precache-manifest.f99f9825b6561112f39436183df9e1b3.js
+++ precache-manifest.f99f9825b6561112f39436183df9e1b3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b426644a3de88d5f3160e39c0e4a8748",
+    "revision": "1e3f32db4b5c79706004551424a0196c",
     "url": "/cv/index.html"
   },
   {
-    "revision": "6f7308572d3087829bc5",
+    "revision": "9c4e717962fffd12f712",
     "url": "/cv/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "25f7a734ae0fd64c696b",
+    "revision": "b61772b53cf13dafcafa",
     "url": "/cv/static/css/main.98001891.chunk.css"
   },
   {
-    "revision": "6f7308572d3087829bc5",
-    "url": "/cv/static/js/2.9429fbc8.chunk.js"
+    "revision": "9c4e717962fffd12f712",
+    "url": "/cv/static/js/2.88bea8c4.chunk.js"
   },
   {
-    "revision": "25f7a734ae0fd64c696b",
-    "url": "/cv/static/js/main.ad25572b.chunk.js"
+    "revision": "b61772b53cf13dafcafa",
+    "url": "/cv/static/js/main.8f5c4844.chunk.js"
   },
   {
     "revision": "a1ec7a6a8174e7aa9843",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.f8b7c6e0a1b9a5b5fc45f015352f8c52.js"
+  "/cv/precache-manifest.f99f9825b6561112f39436183df9e1b3.js"
 );
 
 self.addEventListener('message', (event) => {
