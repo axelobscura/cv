883962833d3f25cd815071578b526a58663fdb22
Axel Obscura <axosar@gmail.com>
2020-04-23 17:01:41 -0500

    Mensaje

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 import './Menu.css';
 
 const Menu = (props) => {
-    return(
+    return (
         <div className="menu">
             <ul>
-                <li><Link to="/"><i className="material-icons">change_history</i></Link></li>
+                <li><Link to="/"><i className="material-icons">change_history____</i></Link></li>
                 <li><Link to="/datos"><i className="material-icons">eco</i></Link></li>
                 <li><Link to="/technical-full-stack"><i className="material-icons">donut_large</i></Link></li>
                 <li><Link to="/datos"><i className="material-icons">explore</i></Link></li>
                 <li><Link to="/datos"><i className="material-icons">fingerprint</i></Link></li>
             </ul>
-            
         </div>
     )
 }
