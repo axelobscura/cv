c8466318e7ae382176d9ddbabe242d08b4a12f5a
Axel Obscura <axosar@gmail.com>
2020-05-18 17:39:51 -0500

    Favicon url

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import './Menu.css';
 
@@ -12,15 +11,15 @@ const Menu = () => {
             <Container className="datos personales">
                 <Row>
                     <Col>
-                        <Nav.Link exact={true} href="/" style={{ paddingLeft: '0' }}>
+                        <Nav.Link href="/" style={{ paddingLeft: '0' }}>
                             <img src="images/profile.jpeg" alt="Full Stack Developer" title="Full Stack Developer" />
                         </Nav.Link>
                         <h1>Axel L.<br />Obscura Sarzotti</h1>
                         <h2>Full Stack Developer</h2>
                         <hr />
-                        <p className="text-left"><i class="material-icons">call</i> (52)(55) 3220-6321</p>
-                        <p className="text-left"><i class="material-icons">email</i> axosar@gmail.com</p>
-                        <p className="text-left"><i class="material-icons">directions</i> Mexico City</p>
+                        <p className="text-left"><i className="material-icons">call</i> (52)(55) 3220-6321</p>
+                        <p className="text-left"><i className="material-icons">email</i> axosar@gmail.com</p>
+                        <p className="text-left"><i className="material-icons">directions</i> Mexico City</p>
                     </Col>
                 </Row>
             </Container>
@@ -34,9 +33,9 @@ const Menu = () => {
             </Nav>
             <Container className="redessociales">
                 <Row style={{ display: 'none' }}>
-                    <Col><i class="material-icons">twitter</i></Col>
-                    <Col><i class="material-icons">facebook</i></Col>
-                    <Col><i class="material-icons">linkedin</i></Col>
+                    <Col><i className="material-icons">twitter</i></Col>
+                    <Col><i className="material-icons">facebook</i></Col>
+                    <Col><i className="material-icons">linkedin</i></Col>
                 </Row>
             </Container>
         </div>
