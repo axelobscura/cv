30730a6e557261ed0ae385b1badee97aa049749c
Axel Obscura <axosar@gmail.com>
2020-05-18 18:35:54 -0500

    Landing page styles

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
 import './Intro.css';
 
 export default () => (
     <Container className="intro" fluid>
         <Row className="m-0 p-0">
             <Col className="m-0 p-0">
-                <h1>Welcome</h1>
+                <h1>Welcome!</h1>
+                <h2>I'm Axel Laurent</h2>
                 <hr />
-                <p>Hi!, I'm a Senior Full Stack Web Developer with 14 years of experience in designing architectures, developing decoupled and front to back web applications using HTML5, CSS3, JavaScript, AJAX, Json, XML, Node, Express, PHP, MySQL, Mongo, Postgress, Angular, React, React Native, Vue, Unit Tests, Jest, Mocha...</p>
+                <p>Senior Full stack JavaScript developer with hands on<br />experience of designing, developing and maintaining web apps as well as mobile applications.<br />Possesses diverse experience of creating multiple highly scalable applications <b>using different tech stacks</b></p>
+                <Link to="/professional-summary"><button className="btn">Professional Summary</button></Link>
             </Col>
         </Row>
     </Container>
