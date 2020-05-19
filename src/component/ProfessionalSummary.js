8231e27e43851752bd57645fed6d741e62ab7bab
Axel Obscura <axosar@gmail.com>
2020-05-19 14:48:36 -0500

    Skills improved

--- src/component/ProfessionalSummary.js
+++ src/component/ProfessionalSummary.js
@@ -12,15 +12,44 @@ export default () => (
                 <h1><span class="material-icons">location_searching</span> Professional Summary</h1>
                 <hr />
                 <p>Full stack JavaScript developer with hands on experience of designing, developing and maintaining web as well as mobile applications. Possesses diverse experience of creating multiple highly scalable applications <b>using different tech stacks</b>. Adept at understanding the requirements of the clients and delivering projects accordingly. Demonstrates the capability of maintaining code by fixing bugs as well as conducting end to end testing. Highly skilled at collaborating with team members to ship beautiful products within deadlines. Looking forward to applying the acquired gamut of skills in a challenging role.</p>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
                 <hr />
+            </Col>
+
+        </Row>
+        <Row className="iconosDev">
+            <Col sm={3}>
                 <h1><span class="material-icons">brightness_high</span> Skills</h1>
-                <hr />
+            </Col>
+            <Col sm={9}>
+                <i class="devicon-javascript-plain"></i>
+                <i class="devicon-typescript-plain"></i>
+                <i class="devicon-react-original"></i>
+                <i class="devicon-angularjs-plain"></i>
+                <i class="devicon-vuejs-plain"></i>
+                <i class="devicon-git-plain"></i>
+                <i class="devicon-jquery-plain"></i>
+                <i class="devicon-html5-plain"></i>
+                <i class="devicon-jasmine-plain"></i>
+                <i class="devicon-bootstrap-plain"></i>
+                <i class="devicon-css3-plain"></i>
+                <i class="devicon-nodejs-plain"></i>
+                <i class="devicon-express-original"></i>
+                <i class="devicon-php-plain"></i>
+                <i class="devicon-trello-plain"></i>
+                <i class="devicon-visualstudio-plain"></i>
+                <i class="devicon-webpack-plain"></i>
+                <i class="devicon-wordpress-plain"></i>
+                <i class="devicon-zend-plain"></i>
+                <i class="devicon-mocha-plain"></i>
+                <i class="devicon-laravel-plain"></i>
+                <i class="devicon-less-plain-wordmark"></i>
+                <i class="devicon-mongodb-plain"></i>
+                <i class="devicon-mysql-plain"></i>
+                <i class="devicon-sass-original"></i>
+                <i class="devicon-ssh-plain"></i>
             </Col>
         </Row>
+        <hr />
         <Row>
             <Col sm={3}>
                 <h2><span class="material-icons">keyboard_arrow_right</span> Front End</h2>
@@ -82,17 +111,15 @@ export default () => (
                     <li>Experience in both <b>Client Side</b> and <b>Server Side</b> Programming</li>
                     <li>Background in all phases of software engineering, including Analysis, Design, Coding, Testing and Implementation as well as <b>Agile</b> software development using Github</li>
                     <li>Experience in <b>NodeJS</b> backend programming and <b>Object Oriented Javascript</b></li>
+                    <li>Exxtensive experience building all kinds of web apps with <b>Wordpress</b></li>
                 </ul>
             </Col>
             <Col sm={6}>
                 <ul>
                     <li>Frontend Development Skills in <b>HTML/CSS/Javascript</b> and <b>Responsive Design</b></li>
                     <li>Extensive experience building web apps with frameworks like <b>ReactJS, Angular +2 and VueJS</b></li>
-                    <li>PHP</li>
-                    <li>.NET</li>
-                    <li>SQL, MongoDB, postgress</li>
-                    <li>Flux Pattern</li>
-                    <li>Model-View-Controller</li>
+                    <li>Task and project management with Jira, BitBucket, Git</li>
+                    <li>Develop and design new functionality and features inline with <b>functional specifications and business requirements</b></li>
                 </ul>
             </Col>
         </Row>
