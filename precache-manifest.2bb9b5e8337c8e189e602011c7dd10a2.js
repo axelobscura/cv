085038b810a00c79c0e02f12a164ac8d1e5467d0
axelobscura <axosar@gmail.com>
2020-09-16 17:05:07 -0500

    Updates

--- precache-manifest.2bb9b5e8337c8e189e602011c7dd10a2.js
+++ precache-manifest.2bb9b5e8337c8e189e602011c7dd10a2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f13040c6a926a0d6d375253977c2ce73",
+    "revision": "f00bbe0d159149ca10bd8ba8654937ef",
     "url": "/index.html"
   },
   {
-    "revision": "3e10d0eff756cf21697c",
+    "revision": "7bec91139c0d24b56175",
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "d5f04ce247301e3742b2",
+    "revision": "a44d661a399f98b9f2fb",
     "url": "/static/css/main.f964e105.chunk.css"
   },
   {
-    "revision": "3e10d0eff756cf21697c",
-    "url": "/static/js/2.c3ffda31.chunk.js"
+    "revision": "7bec91139c0d24b56175",
+    "url": "/static/js/2.3ea71370.chunk.js"
   },
   {
-    "revision": "d5f04ce247301e3742b2",
-    "url": "/static/js/main.13586201.chunk.js"
+    "revision": "a44d661a399f98b9f2fb",
+    "url": "/static/js/main.03be78d4.chunk.js"
   },
   {
     "revision": "2df4d280065006baff63",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bd3c0c4b3227d3631678fb349591fb8.js"
+  "/precache-manifest.2bb9b5e8337c8e189e602011c7dd10a2.js"
 );
 
 self.addEventListener('message', (event) => {
