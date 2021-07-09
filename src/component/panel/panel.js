d822489affe3c042fc52958a7aff39706aa259f3
Axel <axosar@gmail.com>
2021-07-09 17:15:04 -0500

    Passing data to panel

--- src/component/panel/panel.js
+++ src/component/panel/panel.js
@@ -13,6 +13,14 @@ const Panel = (props) => {
       >
         <div>
           <h2>COMPANY: {props.nombre}</h2>
+          <hr/>
+          <h3>RESPONSIBILITIES</h3>
+          <hr/>
+          <ul>
+            <li>Involved in documentation of the architecture and configuration details</li>
+            <li>Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts</li>
+            <li>Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS</li>
+          </ul>
         </div>
       </SlidingPane>
     </div>
