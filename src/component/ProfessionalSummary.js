8d1d4a05d1d01108518de87683465cecb2c179fb
Axel <axosar@gmail.com>
2021-07-09 16:09:24 -0500

    Panel styles

--- src/component/ProfessionalSummary.js
+++ src/component/ProfessionalSummary.js
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Panel from './panel/panel';
 import './ProfessionalSummary.css';
 
-import {work} from '../data/empleos.json';
+import { work } from '../data/empleos.json';
 
-let openDroper = (e) => {
-    console.log(e);
-}
+const ProfessionalSummary = () => {
 
-export default () => (
-    <Container className="ps pro">
-        <Row>
-            <Col>
-                <hr />
-                <h1><span className="material-icons">location_searching</span> Professional Summary</h1>
-                <hr />
-                <Panel />
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                {work.map((empleo, i) => (
-                    <div key={i}>
-                        <h2>
-                            <span className="material-icons droper" onClick={() => openDroper(empleo.empresa)}>keyboard_arrow_right</span> 
-                            {empleo.empresa} | {empleo.periodo}
-                        </h2>
-                        <h3><b>{empleo.rol}</b></h3>
-                        <h3>{empleo.client}</h3>
-                        <hr/>
-                    </div>
-                ))}
-            </Col>
-        </Row>
-    </Container>
-);
+    const [state, setState] = useState({
+        isPaneOpen: false
+    });
+
+    let openDroper = () => {
+        setState({
+            isPaneOpen: true
+        })
+    }
+
+    return (
+        <Container className="ps pro">
+            <Row>
+                <Col>
+                    <hr />
+                    <h1><span className="material-icons">location_searching</span> Professional Summary</h1>
+                    <hr />
+                    <Panel
+                        isPaneOpen={state.isPaneOpen}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {work.map((empleo, i) => (
+                        <div key={i}>
+                            <h2>
+                                <span
+                                    className="material-icons droper"
+                                    onClick={() => openDroper()}
+                                >
+                                    keyboard_arrow_right
+                                </span>
+                                {empleo.empresa} | {empleo.periodo}
+                            </h2>
+                            <h3><b>{empleo.rol}</b></h3>
+                            <h3>{empleo.client}</h3>
+                            <hr />
+                        </div>
+                    ))}
+                </Col>
+            </Row>
+        </Container>
+    )
+};
+
+export default ProfessionalSummary;
--- src/component/panel/panel.js
+++ src/component/panel/panel.js
@@ -1,45 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 
-const Panel = () => {
-  const [state, setState] = useState({
-    isPaneOpen: false,
-    isPaneOpenLeft: false,
-  });
-
+const Panel = (props) => {
+  console.log('estado del panel: ' + props.isPaneOpen)
   return (
     <div>
-      <button onClick={() => setState({ isPaneOpen: true })}>
-        Click me to open right pane!
-      </button>
-      <div style={{ marginTop: "32px" }}>
-        <button onClick={() => setState({ isPaneOpenLeft: true })}>
-          Click me to open left pane with 20% width!
-        </button>
-      </div>
       <SlidingPane
         className="some-custom-class"
         overlayClassName="some-custom-overlay-class"
-        isOpen={state.isPaneOpen}
-        title="Hey, it is optional pane title.  I can be React component too."
-        subtitle="Optional subtitle."
-        onRequestClose={() => {
-          // triggered on "<" on left top click or on outside click
-          setState({ isPaneOpen: false });
-        }}
+        isOpen={props.isPaneOpen}
       >
         <div>And I am pane content. BTW, what rocks?</div>
-      </SlidingPane>
-      <SlidingPane
-        closeIcon={<div>Some div containing custom close icon.</div>}
-        isOpen={state.isPaneOpenLeft}
-        title="Hey, it is optional pane title.  I can be React component too."
-        from="left"
-        width="200px"
-        onRequestClose={() => setState({ isPaneOpenLeft: false })}
-      >
-        <div>And I am pane content on left.</div>
       </SlidingPane>
     </div>
   );
