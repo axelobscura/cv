da007a8753cde25387dcec4883ba715f9da9230e
Axel <axosar@gmail.com>
2021-07-09 10:25:05 -0500

    contact details

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,7 +6,7 @@ import './ProfessionalSummary.css';
 
 const Contact = () => {
     return (
-        <Container className="ps ts">
+        <Container className="ps ts contacto">
             <Row>
                 <Col>
                     <hr />
@@ -15,17 +15,14 @@ const Contact = () => {
                     <Row>
                         <Col>
                             <hr />
-                                <h1><span class="material-icons">coffee</span> Contact</h1>
+                                <h1><span class="material-icons">mail</span> Email</h1>
+                                <h3><a href="mailto:axosar@gmail.com">axosar@gmail.com</a></h3>
                             <hr />
                         </Col>
                         <Col>
                             <hr />
-                                <h1><span class="material-icons">coffee</span> Contact</h1>
-                            <hr />
-                        </Col>
-                        <Col>
-                            <hr />
-                                <h1><span class="material-icons">coffee</span> Contact</h1>
+                                <h1><span class="material-icons">call</span> Phone</h1>
+                                <h3><a href="tel:525532206321">(52) 55 3220 6321</a></h3>
                             <hr />
                         </Col>
                     </Row>
